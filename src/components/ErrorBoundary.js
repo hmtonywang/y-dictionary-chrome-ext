43c5ea43dafcdad848a75845f58e4df50a6e32db
Curtis Wang <hungming.curtis.wang@gmail.com>
2024-08-09 20:30:06 +0800

    Replace <h1> with <p> in ErrorBoundary

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -19,7 +19,7 @@ class ErrorBoundary extends React.Component {
   render() {
     if (this.state.hasError) {
       // You can render any custom fallback UI
-      return <h1>Something went wrong.</h1>;
+      return <p>Something went wrong.</p>;
     }
 
     return this.props.children;
